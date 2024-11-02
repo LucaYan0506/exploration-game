efc1352d045d3483c83393fd75313464ba465e6a
Rongman XU <xurongman2005@126.com>
2024-11-02 19:52:29 +0000

    'init'

--- background.gd
+++ background.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
 #@export var target_level : PackedScene
-
-func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+func _on_pause_pressed() -> void:
+	get_tree().change_scene_to_file("res://pause_menu.tscn")
