40fbc345765157f61be27cacd7b4ec8144c72a98
Rongman XU <xurongman2005@126.com>
2024-11-03 09:40:27 +0000

    add image of gloves

--- scene/slot.gd
+++ scene/slot.gd
@@ -1,13 +1,14 @@
 extends Panel
 
 @onready var backgroundsprite: Sprite2D = $background
-@onready var itemSprite: Sprite2D =$CenterContainer/Panel/item
+@onready var itemSprite: Sprite2D = $CenterContainer/Panel/item
 
-func update(item: InventoryItem):
-	if !item:
+# 更新道具显示状态
+func update(item: InventoryItem) -> void:
+	if item == null:  # 如果 item 参数为空
 		backgroundsprite.frame = 0
 		itemSprite.visible = false
-	else:
+	else:  # 如果 item 有内容
 		backgroundsprite.frame = 1
 		itemSprite.visible = true
-		itemSprite.texture = item.texture
+		itemSprite.texture = item.texture  # 设置 item 的纹理
